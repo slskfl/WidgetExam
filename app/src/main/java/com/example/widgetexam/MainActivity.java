08f552530c76571574c92e41c4ecb4e37bd75723
slskfl <62872578+slskfl@users.noreply.github.com>
2021-01-07 14:53:12 +0900

    Update MainActivity.java

--- app/src/main/java/com/example/widgetexam/MainActivity.java
+++ app/src/main/java/com/example/widgetexam/MainActivity.java
@@ -10,5 +10,24 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    btnShow=findViewById(R.id.btnShow);
+        btnWho=findViewById(R.id.btnWho);
+        ivjeju=findViewById(R.id.ivjeju);
+
+        btnShow.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ivjeju.setImageResource(R.drawable.jeju);
+            }
+        });
+
+        btnWho.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                btnWho.setText("버튼");
+                btnWho.setTextColor(Color.YELLOW);
+                btnShow.setEnabled(true);
+            }
+        });
     }
 }
